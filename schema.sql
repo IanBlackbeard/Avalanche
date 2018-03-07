4cdc834e942736fe51ce7e73050688794f4538a0
Sherry Fitzpatrick <sfitz3141@gmail.com>
2018-03-07 12:47:30 -0700

    Fixed database

--- schema.sql
+++ schema.sql
@@ -1,23 +1,27 @@
+-- DROP DATABASE IF EXISTS avalanche;
+
 CREATE DATABASE avalanche;
 USE avalanche;
 
 CREATE TABLE gear
 (
-	id int NOT NULL AUTO INCREMENT,
+	id int NOT NULL AUTO_INCREMENT,
 	item varchar(30) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE obstacles
 (
-	id int NOT NULL AUTO INCREMENT,
+	id int NOT NULL AUTO_INCREMENT,
 	obstacle varchar(30) NOT NULL,
+	delay int NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE characters
 (
-	id int NOT NULL AUTO INCREMENT,
-	name
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(30) NOT NULL,
+	PRIMARY KEY (id)
 );
 
